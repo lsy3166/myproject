75b91179c19cd5460bc398ccceacfee70391deea
LSY3166\user <lsy3166@gmail.com>
2021-03-26 14:52:02 +0900

    db board에서 delete시 체크된 목록 보여주기

--- src/views/DbBoard.vue
+++ src/views/DbBoard.vue
@@ -10,23 +10,47 @@
       class="elevation-1"
     >
       <template v-slot:top>
-        <v-toolbar>
-          <v-dialog v-model="dialogDelete" max-width="500px">
-            <v-card>
-              <v-card-title class="headline"
-                >Are you sure you want to delete these checked items?</v-card-title
-              >
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-                <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
-                <v-spacer></v-spacer>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-toolbar>
         <v-container>
           <v-card flat>
+            <v-toolbar height="0px">
+              <v-dialog v-model="dialogDelete" max-width="500px">
+                <v-card>
+                  <v-card-title class="headline"
+                    >Are you sure you want to delete these checked items?</v-card-title
+                  >
+                  <v-divider></v-divider>
+                  <v-list class="transparent">
+                    <v-list-item v-for="item in selected" :key="item._id">
+                      <v-list-item-icon>
+                        <v-icon>mdi-circle-small</v-icon>
+                      </v-list-item-icon>
+                      <v-list-item-title>{{ item.title }}</v-list-item-title>
+
+                      <v-list-item-subtitle class="text-right">
+                        {{ item.writer }}
+                      </v-list-item-subtitle>
+                    </v-list-item>
+                  </v-list>
+                  <v-divider></v-divider>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                    <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                    <v-spacer></v-spacer>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+              <v-dialog v-model="dialogDetail" max-width="500px">
+                <v-card>
+                  <v-card-title class="headline">Detail</v-card-title>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="closeDetail">Close</v-btn>
+                    <v-spacer></v-spacer>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-toolbar>
             <v-row align="center">
               <v-switch v-model="singleSelect" label="Single select" class="pa-3"></v-switch>
               <v-spacer></v-spacer>
@@ -71,6 +95,7 @@ export default {
     return {
       singleSelect: false,
       dialogDelete: false,
+      dialogDetail: false,
       selected: [],
       headers: [
         {
@@ -105,6 +130,9 @@ export default {
   watch: {
     dialogDelete(val) {
       val || this.closeDelete();
+    },
+    dialogDetail(val) {
+      val || this.closeDetail();
     }
   },
   methods: {
@@ -122,11 +150,16 @@ export default {
       this.boards = await api.getboards();
     },
     deleteRows() {
-      this.dialogDelete = true;
+      if (this.selected.length == 0) {
+        alert('There is no selected item!');
+      } else {
+        this.dialogDelete = true;
+      }
     },
     async detailItem(item) {
       item.count += 1;
       await api.updateboard(item, item._id);
+      this.dialogDetail = true;
     },
     getColor(calories) {
       if (calories > 10) return 'red';
@@ -146,6 +179,9 @@ export default {
     },
     closeDelete() {
       this.dialogDelete = false;
+    },
+    closeDetail() {
+      this.dialogDetail = false;
     }
   }
 };
